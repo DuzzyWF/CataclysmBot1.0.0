a225c4d137d920e4b6625cef3050e5d31b59b32f
Eric Rocha <ericrochaeric@gmail.com>
2022-01-07 02:23:36 -0300

    add response to suggest

---
+++ commands/responder.ts
@@ -0,0 +1,65 @@
+import { Message, MessageEmbed, Snowflake, TextChannel } from "discord.js";
+
+const prefix = "!";
+
+export default {
+  callback: (message: Message) => {
+    message.delete();
+
+    const error = new MessageEmbed().setColor("ORANGE").setFields(
+      {
+        name: "Comando incorreto!",
+        value: "Use !responder <id> <resposta>",
+      },
+      {
+        name: "Exemplo:",
+        value: "!responder 000000000000000000 Ótima ideia, será adicionado!a",
+      }
+    );
+
+    const permission = new MessageEmbed().setColor("RED").setFields({
+      name: "Sem permissão!",
+      value: "Você não tem permissão para executar este comando!",
+    });
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+    const command = args.shift()?.toLowerCase();
+
+    const id = args.shift()?.toLowerCase();
+
+    const response = args.join(" ");
+
+    if (!message.member?.permissions.has("MANAGE_GUILD")) {
+      message.channel.send({
+        content: `<@${message.author.id}>`,
+        embeds: [permission],
+      });
+    } else {
+      if ((command === "responder" && !id && !response) || id?.length !== 18) {
+        message.channel.send({
+          content: `<@${message.author.id}>`,
+          embeds: [error],
+        });
+      } else {
+        const adminResponse = new MessageEmbed()
+          .setAuthor({
+            name: message.author.tag,
+            iconURL: message.author.displayAvatarURL({ dynamic: true }),
+          })
+          .setColor("ORANGE")
+          .setDescription(`${response}`);
+
+        const channel = message.guild?.channels.cache.get(
+          "925459036432920616"
+        ) as TextChannel;
+
+        channel.messages.fetch(id).then((msg) => {
+          const playerEmbeds = msg.embeds[0];
+          msg.edit({ embeds: [playerEmbeds, adminResponse] });
+          msg.reactions.removeAll();
+        });
+      }
+    }
+  },
+};
--- commands/sugerir.ts
+++ commands/sugerir.ts
@@ -1,47 +1,51 @@
 import { Message, MessageEmbed, TextChannel } from "discord.js";
 
-const prefix = '!';
+const prefix = "!";
 
 export default {
+  callback: (message: Message) => {
+    message.delete();
 
-    callback: (message: Message) => {
+    const error = new MessageEmbed().setColor("ORANGE").setFields(
+      {
+        name: "Comando incorreto!",
+        value: "Use !sugerir <sugestão>",
+      },
+      {
+        name: "Exemplo:",
+        value: "!sugerir Adicionar mais itens no mercado",
+      }
+    );
 
-        message.delete();
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
 
-        const error = new MessageEmbed()
-        .setColor('ORANGE')
-        .setFields(
-            {
-                name: 'Comando incorreto!',
-                value: 'Use !sugerir <sugestão>'
-            },
-            {
-                name: 'Exemplo:',
-                value: '!sugerir Adicionar mais itens no mercado'
-            }
-        )
+    const command = args.shift()?.toLowerCase();
 
-        const args = message.content.slice(prefix.length).trim().split(/ +/g);
-        const command = args.shift()?.toLowerCase();
+    const sugest = args.join(" ");
 
-        const sugest = args.join(' ');
+    if (command === "sugerir" && !sugest) {
+      message.channel.send({
+        content: `<@${message.author.id}>`,
+        embeds: [error],
+      });
+    } else {
+      const playerSugestion = new MessageEmbed()
+        .setAuthor({
+          name: message.author.tag,
+          iconURL: message.author.displayAvatarURL({ dynamic: true }),
+        })
+        .setColor("ORANGE")
+        .setDescription(`${sugest}`);
 
-        if (command === 'sugerir' && !sugest) {
-            message.channel.send({ content: `<@${message.author.id}>`, embeds: [error] })
-        }
-
-        else {
-
-            const playerSugestion = new MessageEmbed()
-            .setAuthor({ name: message.author.tag, iconURL: message.author.displayAvatarURL({dynamic: true}) })
-            .setColor('ORANGE')
-            .setDescription(`${sugest}`)
-            
-            const channel = message.guild?.channels.cache.get('925459036432920616') as TextChannel
-            channel.send({ embeds: [playerSugestion] }).then((message) => {
-                message.react('✅'),
-                message.react('❎');
-            })
-        }
-    },
-}
+      const channel = message.guild?.channels.cache.get(
+        "925459036432920616"
+      ) as TextChannel;
+      channel.send({ embeds: [playerSugestion] }).then((message) => {
+        message.edit({
+          embeds: [playerSugestion.addField("ID da sugestão:", message.id)],
+        });
+        message.react("✅"), message.react("❎");
+      });
+    }
+  },
+};
